0779751c4649696bddad83e7380602ecdbcf5127
Víctor Manuel Fernández Castro <vmfdez90@gmail.com>
2015-12-19 15:57:02 +0100

    Paneles de archivo y alisamiento.

--- p3/practica3.m
+++ p3/practica3.m
@@ -22,7 +22,7 @@ function varargout = practica3(varargin)
 
 % Edit the above text to modify the response to help practica3
 
-% Last Modified by GUIDE v2.5 19-Dec-2015 15:15:21
+% Last Modified by GUIDE v2.5 19-Dec-2015 15:42:24
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -54,6 +54,16 @@ function practica3_OpeningFcn(hObject, eventdata, handles, varargin)
 
 % Choose default command line output for practica3
 handles.output = hObject;
+
+set(handles.btOpen, 'cdata', imresize(imread('res/box.png'), [24 24]));
+set(handles.btSave, 'cdata', imresize(imread('res/floppy.png'), [24 24]));
+handles.im = cell(1, 3);
+handles.axes = {handles.axes1 handles.axes2 handles.axes3};
+
+for i = 1:3
+    axes(handles.axes{i});
+    imshow([]);
+end
 
 % Update handles structure
 guidata(hObject, handles);
@@ -105,10 +115,48 @@ function btSmooth_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
+if (size(handles.im{1}) == [0 0])
+    errordlg('No hay imagen cargada.');
+    return
+end
+
+[s status] = str2num(get(handles.inSmoothSize, 'String'));
+
+if status == 0 || s < 1
+    errordlg('Tamaño incorrecto.');
+    return
+end
+
+switch get(handles.menuSmooth, 'Value')
+    case 1
+        h = fspecial('average', s);
+        handles.im{1} = imfilter(handles.im{1}, h, 'conv');
+    case 2
+        [sigma, status] = str2num(get(handles.inSmoothSigma, 'String'));
+        
+        if status == 0 || s < 0
+            errordlg('Desviación incorrecta.');
+            return
+        end
+        
+        h = fspecial('gaussian', s, sigma);
+        handles.im{1} = imfilter(handles.im{1}, h, 'conv');
+    case 3
+        handles.im{1} = medfilt2(handles.im{1}, [s s]);
+    case 4
+        handles.im{1} = ordfilt2(handles.im{1}, 1, ones(s));
+    case 5
+        handles.im{1} = ordfilt2(handles.im{1}, s * s, ones(s));
+end
+
+axes(handles.axes{1});
+imshow(handles.im{1}, []);
+guidata(hObject, handles);
+
 
 % --- Executes during object creation, after setting all properties.
-function inSmoothDeviation_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to inSmoothDeviation (see GCBO)
+function inSmoothSigma_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to inSmoothSigma (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
@@ -125,12 +173,46 @@ function btOpen_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
+filename = imgetfile;
+
+if (filename)
+    im = imread(filename);
+    
+    if (ndims(im) > 2)
+        im = rgb2gray(im);
+    end
+
+    handles.im{1} = im;
+    axes(handles.axes{1});
+    imshow(im, []);
+    guidata(hObject, handles);
+end
+
 
 % --- Executes on button press in btSave.
 function btSave_Callback(hObject, eventdata, handles)
 % hObject    handle to btSave (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+if get(handles.btFile1, 'Value')
+    ifile = 1;
+elseif get(handles.btFile2, 'Value')
+    ifile = 2;
+else
+    ifile = 3;
+end
+
+if size(handles.im{ifile}) == [0 0]
+    errordlg('No hay imagen para guardar.');
+    return
+end
+
+filename = imputfile;
+
+if (filename)
+    imwrite(handles.im{ifile}, filename);
+end
 
 
 % --- Executes during object creation, after setting all properties.
@@ -236,4 +318,50 @@ function menuGradient_CreateFcn(hObject, eventdata, handles)
 %       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in bxAutoThreshold.
+function bxAutoThreshold_Callback(hObject, eventdata, handles)
+% hObject    handle to bxAutoThreshold (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of bxAutoThreshold
+
+if get(hObject, 'Value')
+    set(handles.inEdgeThreshold, 'Enable', 'off');
+else
+    set(handles.inEdgeThreshold, 'Enable', 'on');
+end
+
+
+% --- Executes on button press in bxRemoveNoMaximum.
+function bxRemoveNoMaximum_Callback(hObject, eventdata, handles)
+% hObject    handle to bxRemoveNoMaximum (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of bxRemoveNoMaximum
+
+if get(hObject, 'Value')
+    set(handles.inRemoveSize, 'Enable', 'on');
+else
+    set(handles.inRemoveSize, 'Enable', 'off');
+end
+
+
+% --- Executes on selection change in menuSmooth.
+function menuSmooth_Callback(hObject, eventdata, handles)
+% hObject    handle to menuSmooth (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns menuSmooth contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from menuSmooth
+
+if get(hObject, 'Value') == 2
+    set(handles.inSmoothSigma, 'Enable', 'on');
+else
+    set(handles.inSmoothSigma, 'Enable', 'off');
 end
